ce43154c94a441e2dc303cbb4345bab114f0b221
kiart-tantasi <kiarttantasi@gmail.com>
2025-08-18 16:47:53 +0700

    add argument for output filename

--- main.go
+++ main.go
@@ -7,28 +7,45 @@ import (
 	"strconv"
 )
 
-const USAGE_MESSAGE = "Usage: go run main.go <count, required> <domain, optional>"
+const USAGE_MESSAGE = "Usage: go run main.go <count, required> <domain, optional> <output-filename, optional>"
 
 func main() {
-	// Check if a command-line argument was provided
+	// ========================== ARGUMENTS ==========================
+	// Basic checks
 	if len(os.Args) < 2 {
 		fmt.Println(USAGE_MESSAGE)
 		os.Exit(1)
-	} else if os.Args[1] == "help" {
+	}
+	// help
+	if os.Args[1] == "help" {
 		fmt.Println(USAGE_MESSAGE)
 		os.Exit(0)
 	}
-
-	// Parse the first argument as an integer
+	// 1st argument: count
 	count, err := strconv.Atoi(os.Args[1])
 	if err != nil {
 		fmt.Printf("Error with invalid count argument: %s\n", err)
 		os.Exit(1)
 	}
+	// 2nd argument: email domain
+	domain := ""
+	if len(os.Args) > 2 && os.Args[2] != "" {
+		domain = os.Args[2]
+	} else {
+		domain = fmt.Sprintf("loadtest%d.com", count)
+	}
+	fmt.Printf("Will use domain %s\n", domain)
+	// 3rd argument: output filename
+	outputFilename := ""
+	if len(os.Args) > 3 && os.Args[3] != "" {
+		outputFilename = os.Args[3]
+	} else {
+		outputFilename = fmt.Sprintf("contacts%d.csv", count)
+	}
+	// ========================== ARGUMENTS ==========================
 
-	// Create or overwrite the file
-	fileName := fmt.Sprintf("contacts%d.csv", count)
-	file, err := os.Create(fileName)
+	// Create output file
+	file, err := os.Create(outputFilename)
 	if err != nil {
 		fmt.Println("Error creating file:", err)
 		return
@@ -44,15 +61,6 @@ func main() {
 		fmt.Println("Error writing to file:", err)
 		return
 	}
-
-	// Define domain based on argument indexed 2
-	domain := ""
-	if len(os.Args) > 2 && os.Args[2] != "" {
-		domain = os.Args[2]
-	} else {
-		domain = fmt.Sprintf("loadtest%d.com", count)
-	}
-	fmt.Printf("Will use domain %s\n", domain)
 
 	// Use buffered writer for performance
 	for i := 1; i <= count; i++ {
@@ -70,5 +78,5 @@ func main() {
 		return
 	}
 
-	fmt.Println("Done! Wrote", count, "lines to", fileName)
+	fmt.Println("Done! Wrote", count, "lines to", outputFilename)
 }
